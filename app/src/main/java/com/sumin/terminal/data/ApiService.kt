acc4271cc00e877baf5cd78f5fd20ff64dc55ff9
sumin93 <suminfx@mail.ru>
2023-09-13 21:45:34 +0300

    Scaling

--- app/src/main/java/com/sumin/terminal/data/ApiService.kt
+++ app/src/main/java/com/sumin/terminal/data/ApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=Is6w5efIZ0AYP5cf1wBPQCo4Fo4LXobp")
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=Is6w5efIZ0AYP5cf1wBPQCo4Fo4LXobp")
     suspend fun loadBars(): Result
 }
--- app/src/main/java/com/sumin/terminal/presentation/Terminal.kt
+++ app/src/main/java/com/sumin/terminal/presentation/Terminal.kt
@@ -2,26 +2,45 @@ package com.sumin.terminal.presentation
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import com.sumin.terminal.data.Bar
+import kotlin.math.roundToInt
+
+private const val MIN_VISIBLE_BARS_COUNT = 20
 
 @Composable
 fun Terminal(bars: List<Bar>) {
+    var visibleBarsCount by remember {
+        mutableStateOf(100)
+    }
+
+    val transformableState = TransformableState { zoomChange, _, _ ->
+        visibleBarsCount = (visibleBarsCount / zoomChange).roundToInt()
+            .coerceIn(MIN_VISIBLE_BARS_COUNT, bars.size)
+    }
+
     Canvas(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
+            .transformable(transformableState)
     ) {
         val max = bars.maxOf { it.high }
         val min = bars.minOf { it.low }
-        val barWidth = size.width / bars.size
+        val barWidth = size.width / visibleBarsCount
         val pxPerPoint = size.height / (max - min)
-        bars.forEachIndexed { index, bar ->
-            val offsetX = index * barWidth
+        bars.take(visibleBarsCount).forEachIndexed { index, bar ->
+            val offsetX = size.width - index * barWidth
             drawLine(
                 color = Color.White,
                 start = Offset(offsetX, size.height - ((bar.low - min) * pxPerPoint)),
